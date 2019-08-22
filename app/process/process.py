fd9664257d544d5922bc92c04e5c8f9f0a53d417
Rafael Schlatter <rafaelschlatter@gmail.com>
2019-08-22 15:20:38 +0200

    Flash for processing.

--- app/process/process.py
+++ app/process/process.py
@@ -1,11 +1,11 @@
 import os
-from flask import render_template, current_app, redirect, url_for
+from flask import render_template, current_app, redirect, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
 from app.process import process_bp
 
 
-@process_bp.route("/process")
+@process_bp.route("/process", methods=["GET", "POST"])
 def process():
     blob_names = _list_images(
         block_blob_service=current_app.config["BLOCK_BLOB_SERVICE"]
@@ -15,12 +15,19 @@ def process():
 
     if form.validate_on_submit():
         image = dict(form.selection.choices).get(form.selection.data)
+        filepath = os.path.join(current_app.config["TEMP_DIR"], str(image)
         current_app.config["BLOCK_BLOB_SERVICE"].get_blob_to_path(
             container_name="imageinput",
             blob_name=str(image),
-            file_path=os.path.join(current_app.config["TEMP_DIR"], str(image)),
+            file_path=filepath),
         )
-        return redirect(url_for("index.index"))
+
+        image_for_processing = filepath
+        # Here needs to come the call to run() method from the algorithm class engineering-drawing-cv
+        # The image above 
+
+        flash("Image processed successfully.", "success")
+        return redirect(url_for("process.process"))
 
     return render_template("process_form.html", form=form)
 
