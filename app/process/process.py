1aa8f715521448d0e9783b3d34de5efdf6e2650d
Rafael Schlatter <rafaelschlatter@gmail.com>
2019-08-22 12:44:31 +0200

    Make image selection dynamic as new images get added.

--- app/process/process.py
+++ app/process/process.py
@@ -17,7 +17,9 @@ def process():
         file_path=os.path.join(current_app.config["TEMP_DIR"], "test_separator.png"),
     )
 
+    blob_names = _list_images()
     form = ProcessForm()
+    form.selection.choices = [(index, name) for index, name in enumerate(blob_names)]
     return render_template("process_form.html", form=form)
 
 
@@ -26,8 +28,13 @@ def _list_images():
         account_name=os.environ["STORAGE_ACCOUNT"], account_key=os.environ["BLOB_KEY1"]
     )
     blob_generator = block_blob_service.list_blobs(container_name="imageinput")
+    blob_names = []
+    for blob in blob_generator:
+        blob_names.append(blob.name)
+
+    return blob_names
 
 
 class ProcessForm(FlaskForm):
-    selection = SelectField(u"Select image", choices=[("1", "one"), ("2", "two"), ("3", "three")])
+    selection = SelectField(u"Select image", coerce=int)
     submit = SubmitField("Process image")
