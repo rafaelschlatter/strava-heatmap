4129fb13cf27fc2410d6fa314ac94b56934cd5f7
Rafael Schlatter <rafaelschlatter@gmail.com>
2019-08-22 11:55:34 +0200

    Select input image with dropdown menu.

--- app/process/process.py
+++ app/process/process.py
@@ -1,5 +1,7 @@
 import os
 from flask import render_template, current_app
+from flask_wtf import FlaskForm
+from wtforms import SelectField, SubmitField
 from azure.storage.blob import BlockBlobService
 from app.process import process_bp
 
@@ -15,7 +17,8 @@ def process():
         file_path=os.path.join(current_app.config["TEMP_DIR"], "test_separator.png"),
     )
 
-    return render_template("base.html")
+    form = ProcessForm()
+    return render_template("process_form.html", form=form)
 
 
 def _list_images():
@@ -23,3 +26,8 @@ def _list_images():
         account_name=os.environ["STORAGE_ACCOUNT"], account_key=os.environ["BLOB_KEY1"]
     )
     blob_generator = block_blob_service.list_blobs(container_name="imageinput")
+
+
+class ProcessForm(FlaskForm):
+    selection = SelectField(u"Select image", choices=[("1", "one"), ("2", "two"), ("3", "three")])
+    submit = SubmitField("Process image")
