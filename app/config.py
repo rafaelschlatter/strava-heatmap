f9bec117e0ff408ed34400397e12bf3cb939b469
Rafael Schlatter <rafaelschlatter@gmail.com>
2019-08-22 13:49:11 +0200

    Only one blob service in config.

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 from flask_uploads import UploadSet, IMAGES
+from azure.storage.blob import BlockBlobService
 
 
 class Config(object):
@@ -7,3 +8,6 @@ class Config(object):
     UPLOADED_PHOTOS_DEST = "app/static/input_img"
     TEMP_DIR = "app/static/temp"
     PHOTOS = UploadSet("photos", IMAGES)
+    BLOCK_BLOB_SERVICE = BlockBlobService(
+        account_name=os.environ["STORAGE_ACCOUNT"], account_key=os.environ["BLOB_KEY1"]
+    )
