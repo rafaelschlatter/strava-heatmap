bfe53831964d08fc59803035c0291aaef37993cf
Rafael Schlatter <rafaelschlatter@gmail.com>
2019-08-22 10:41:42 +0200

    Renamed app to application for app service compatibility.

--- app/process/process.py
+++ app/process/process.py
@@ -7,17 +7,19 @@ from app.process import process_bp
 @process_bp.route("/process")
 def process():
     block_blob_service = BlockBlobService(
-            account_name=os.environ["STORAGE_ACCOUNT"],
-            account_key=os.environ["BLOB_KEY1"],
-        )
-    block_blob_service.get_blob_to_path(container_name="imageinput", blob_name="test_separator.png", file_path=os.path.join(current_app.config["TEMP_DIR"], "test_separator.png"))
+        account_name=os.environ["STORAGE_ACCOUNT"], account_key=os.environ["BLOB_KEY1"]
+    )
+    block_blob_service.get_blob_to_path(
+        container_name="imageinput",
+        blob_name="test_separator.png",
+        file_path=os.path.join(current_app.config["TEMP_DIR"], "test_separator.png"),
+    )
 
     return render_template("base.html")
 
 
 def _list_images():
     block_blob_service = BlockBlobService(
-            account_name=os.environ["STORAGE_ACCOUNT"],
-            account_key=os.environ["BLOB_KEY1"],
-        )
+        account_name=os.environ["STORAGE_ACCOUNT"], account_key=os.environ["BLOB_KEY1"]
+    )
     blob_generator = block_blob_service.list_blobs(container_name="imageinput")
