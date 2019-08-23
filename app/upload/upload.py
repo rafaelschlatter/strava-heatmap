e1364aaf359d291d8aff983517edc257c90c3e93
Rafael Schlatter <rafaelschlatter@gmail.com>
2019-08-23 08:21:12 +0200

    Better template for upload form.

--- app/upload/upload.py
+++ app/upload/upload.py
@@ -1,14 +1,18 @@
 import os
 from flask import request, render_template, current_app, flash, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
 from app.upload import upload_bp
 
 
 @upload_bp.route("/upload", methods=["GET", "POST"])
 def upload():
-    if request.method == "POST" and "photo" in request.files:
+    form = UploadForm()
+    if form.validate_on_submit():
         try:
-            filename = current_app.config["PHOTOS"].save(request.files["photo"])
-            image = os.path.join(current_app.config["UPLOADED_PHOTOS_DEST"], filename)
+            f = form.file.data
+            image = os.path.join(current_app.config["UPLOADED_PHOTOS_DEST"], f.filename)
+            form.file.data.save(image)
             _upload_to_blob(
                 image=image, block_blob_service=current_app.config["BLOCK_BLOB_SERVICE"]
             )
@@ -18,7 +22,7 @@ def upload():
             flash("Failed to save image to blob storage.", "danger")
             return redirect(url_for("upload.upload"))
 
-    return render_template("upload.html")
+    return render_template("upload.html", form=form)
 
 
 def _upload_to_blob(image, block_blob_service):
@@ -27,3 +31,8 @@ def _upload_to_blob(image, block_blob_service):
     block_blob_service.create_blob_from_path(
         container_name="imageinput", blob_name=blob_name, file_path=file_path
     )
+
+
+class UploadForm(FlaskForm):
+    file = FileField(label="Select a file")
+    submit = SubmitField(label="Upload image")
