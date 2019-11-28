b95feae2fae5a8987fb6d8efc36a3993904e68f6
Rafael <rafaelschlatter@gmail.com>
2019-11-28 22:26:31 +0100

    Deleted unused stuff

--- app/config.py
+++ app/config.py
@@ -1,13 +1,7 @@
 import os
-from flask_uploads import UploadSet, IMAGES
-from azure.storage.blob import BlockBlobService
 
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    UPLOADED_PHOTOS_DEST = "app/static/input_img"
-    TEMP_DIR = "app/static/temp"
-    PHOTOS = UploadSet("photos", IMAGES)
-    BLOCK_BLOB_SERVICE = BlockBlobService(
-        account_name=os.environ["STORAGE_ACCOUNT"], account_key=os.environ["BLOB_KEY1"]
-    )
+    STRAVA_CLIENT_ID = os.environ.get("STRAVA_CLIENT_ID")
+    STRAVA_CLIENT_SECRET = os.environ.get("STRAVA_CLIENT_SECRET")
