d0eb58f725098f99ae21177162d4f9627ee8daae
Rafael <rafaelschlatter@gmail.com>
2019-11-28 22:26:31 +0100

    Deleted unused stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,15 @@
 from flask import Flask
-from flask_uploads import configure_uploads
 from flask_bootstrap import Bootstrap
 from app.config import Config
 from app.errors import errors_bp
 from app.index import index_bp
-from app.upload import upload_bp
-from app.process import process_bp
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    configure_uploads(app, app.config["PHOTOS"])
     bootstrap = Bootstrap(app)
     app.register_blueprint(errors_bp)
     app.register_blueprint(index_bp)
-    app.register_blueprint(upload_bp)
-    app.register_blueprint(process_bp)
 
     return app
--- app/upload/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-upload_bp = Blueprint("upload", __name__)
-
-from app.upload import upload
