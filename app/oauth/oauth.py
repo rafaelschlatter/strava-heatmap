98f76f925994ab3435e5a70494aec36a146f6d55
Rafael <rafaelschlatter@gmail.com>
2019-12-06 18:33:25 +0100

    Use black formatter

--- app/oauth/oauth.py
+++ app/oauth/oauth.py
@@ -1,15 +1,25 @@
 from stravalib import Client
-from flask import Flask, render_template, redirect, url_for, request, jsonify, current_app
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    url_for,
+    request,
+    jsonify,
+    current_app,
+)
 from app.oauth import oauth_bp
 
 
 @oauth_bp.route("/login")
 def login():
     c = Client()
-    url = c.authorization_url(client_id=current_app.config['STRAVA_CLIENT_ID'],
-                              redirect_uri=url_for('oauth.logged_in', _external=True),
-                              approval_prompt='auto')
-    return render_template('login.html', authorize_url=url)
+    url = c.authorization_url(
+        client_id=current_app.config["STRAVA_CLIENT_ID"],
+        redirect_uri=url_for("oauth.logged_in", _external=True),
+        approval_prompt="auto",
+    )
+    return render_template("login.html", authorize_url=url)
 
 
 @oauth_bp.route("/strava-oauth")
@@ -20,17 +30,21 @@ def logged_in():
     - code
     - error
     """
-    error = request.args.get('error')
-    state = request.args.get('state')
+    error = request.args.get("error")
+    state = request.args.get("state")
     if error:
-        return render_template('login_error.html', error=error)
+        return render_template("login_error.html", error=error)
     else:
-        code = request.args.get('code')
+        code = request.args.get("code")
         client = Client()
-        access_token = client.exchange_code_for_token(client_id=current_app.config['STRAVA_CLIENT_ID'],
-                                                      client_secret=current_app.config['STRAVA_CLIENT_SECRET'],
-                                                      code=code)
+        access_token = client.exchange_code_for_token(
+            client_id=current_app.config["STRAVA_CLIENT_ID"],
+            client_secret=current_app.config["STRAVA_CLIENT_SECRET"],
+            code=code,
+        )
         # Probably here you'd want to store this somewhere -- e.g. in a database.
         strava_athlete = client.get_athlete()
 
-        return render_template('login_results.html', athlete=strava_athlete, access_token=access_token)
+        return render_template(
+            "login_results.html", athlete=strava_athlete, access_token=access_token
+        )
