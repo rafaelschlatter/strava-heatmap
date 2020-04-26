be3ee4a62be62f010bde7b1daf3ae5f7c6cb3952
Rafael Schlatter <rafaelschlatter@gmail.com>
2020-04-26 13:54:16 +0200

    Simplify application, and use plain bootstrap, not flask-bootstrap.

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,11 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from app.config import Config
-from app.errors import errors_bp
 from app.index import index_bp
-from app.oauth import oauth_bp
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    bootstrap = Bootstrap(app)
-    app.register_blueprint(errors_bp)
     app.register_blueprint(index_bp)
-    app.register_blueprint(oauth_bp)
 
     return app
--- app/oauth/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-oauth_bp = Blueprint("oauth", __name__)
-
-from app.oauth import oauth
