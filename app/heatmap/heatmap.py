5526cdf0df9844d0aea3ec5be8fb8e2e33255af4
Rafael Schlatter <rafaelschlatter@gmail.com>
2020-04-27 09:00:52 +0200

    Test with sample map.

---
+++ app/heatmap/heatmap.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from app.heatmap import heatmap_bp
+
+
+@heatmap_bp.route("/heatmap")
+def heatmap():
+    return render_template("map_base.html")
+
+@heatmap_bp.route("/_heatmap")
+def _heatmap():
+    return render_template("map.html")
