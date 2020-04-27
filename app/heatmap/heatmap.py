e058520744c27f4f5690c0c489d7c4397cf542a8
Rafael Schlatter <rafaelschlatter@gmail.com>
2020-04-27 17:40:15 +0200

    Make map fullscreen with button to back home.

--- app/heatmap/heatmap.py
+++ app/heatmap/heatmap.py
@@ -4,7 +4,7 @@ from app.heatmap import heatmap_bp
 
 @heatmap_bp.route("/heatmap")
 def heatmap():
-    return render_template("map_base.html")
+    return render_template("heatmap.html")
 
 @heatmap_bp.route("/_heatmap")
 def _heatmap():
---
+++ scripts/create_heatmap.py
@@ -0,0 +1,34 @@
+import folium
+from flask import url_for
+from branca.element import Template, MacroElement
+
+
+m = folium.Map(
+    name="Strava Heatmap",
+    tiles='cartodbpositron',
+    location=[61.91, 10.76],
+    zoom_start=5.3,
+    control_scale=True
+)
+
+template = """
+{% macro html(this, kwargs) %}
+<!doctype html>
+<html lang="en">
+<body>
+<div id='maplegend' class='maplegend' 
+    style='position: absolute; z-index:9999; border:0px solid grey;; background-color:rgba(255, 255, 255, 0.5);
+     border-radius:6px; padding: 0px; font-size:7px; left: 50px; top: 11px;'>
+     
+<div class='legend-title'><a href="http://heatmap.westeurope.azurecontainer.io/" class="btn btn-outline-dark">Back to home</a></div>
+</div>
+</body>
+</html>
+{% endmacro %}"""
+
+macro = MacroElement()
+macro._template = Template(template)
+
+m.get_root().add_child(macro)
+
+m.save('../app/templates/heatmap.html')
