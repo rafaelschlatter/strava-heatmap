3dca701deb27e65f1731e64a24b88ce8829a8cb8
Rafael Schlatter <rafaelschlatter@gmail.com>
2020-04-28 08:48:29 +0200

    New heatmap, removed old stuff.

--- app/heatmap/heatmap.py
+++ app/heatmap/heatmap.py
@@ -5,7 +5,3 @@ from app.heatmap import heatmap_bp
 @heatmap_bp.route("/heatmap")
 def heatmap():
     return render_template("heatmap.html")
-
-@heatmap_bp.route("/_heatmap")
-def _heatmap():
-    return render_template("map.html")
