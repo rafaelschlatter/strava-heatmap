2f2eba0ca3f630ef84c76992dc433c4084d3b2c4
Rafael Schlatter <rafaelschlatter@gmail.com>
2020-04-29 16:05:13 +0200

    Minor change.

--- app/config.py
+++ app/config.py
@@ -3,8 +3,3 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    STRAVA_CLIENT_ID = os.environ.get("STRAVA_CLIENT_ID") or 29843
-    STRAVA_CLIENT_SECRET = (
-        os.environ.get("STRAVA_CLIENT_SECRET")
-        or "3b927ec04c2212f0fa5ced18b046668acbecc870"
-    )
