6c6fb1ac4adb639ca8cd8ac4e7b6a9b3a25b7fee
Rafael Schlatter <rafaelschlatter@gmail.com>
2020-05-19 07:52:16 +0200

    Better html.

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,13 @@
 from wtforms import Form, StringField
+from flask_wtf import RecaptchaField
 from wtforms.validators import InputRequired, Email
 
 
 class ContactForm(Form):
-    name = StringField("Name", validators=[InputRequired("Please enter your name")])
+    name = StringField("Name", validators=[InputRequired("Please enter your name.")])
     email = StringField("Email", validators=[
-        InputRequired("Please enter your email address"),
-        Email("Please enter a valid email address")]
+        InputRequired("Please enter your email address."),
+        Email("Please enter a valid email address.")]
     )
-    message = StringField("Message", validators=[InputRequired("Please enter a message")])
+    message = StringField("Message", validators=[InputRequired("Please enter a message.")])
+    recaptcha = RecaptchaField("ReCaptcha")
