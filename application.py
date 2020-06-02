64558f32d06eae9fa25e412e1383b0fbb5151726
Rafael Schlatter <rafaelschlatter@gmail.com>
2020-06-02 21:38:25 +0200

    Added favicon to site.

--- application.py
+++ application.py
@@ -4,14 +4,10 @@ from app import create_app
 app = create_app()
 
 
+# Only for debugging purposes
 if __name__ == "__main__":
-    if app.config["ENV"] == "dev":
-        host = "localhost"
-        port = 5000
-        debug = True
-    elif app.config["ENV"] == "prod":
-        host = "0.0.0.0"
-        port = 80
-        debug = False
+    host = "localhost"
+    port = 5000
+    debug = True
 
     app.run(host=host, port=port, debug=debug)
