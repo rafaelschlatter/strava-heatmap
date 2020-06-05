43521ec929ebadc36cb566b2b7818e3a1ba52508
Rafael Schlatter <rafaelschlatter@gmail.com>
2020-06-05 17:29:37 +0200

    Added config.

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    ENV = os.environ.get("ENV")
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("GMAIL_ADRESS")
+    MAIL_PASSWORD = os.environ.get("GMAIL_PW")
+
+    RECAPTCHA_USE_SSL = True
+    RECAPTCHA_PUBLIC_KEY = os.environ.get("RECAPTCHA_PUBLIC_KEY")
+    RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY")
